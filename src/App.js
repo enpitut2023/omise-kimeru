a7a5eab2450c65fd6b699ab5a46c7f41578b80e3
Mattun1212 <s1911437@s.tsukuba.ac.jp>
2023-07-20 14:55:01 +0900

    [ADD]HotpepperAPI

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
         </h1>
         <OmiseList/> {/* <OmiseList></OmiseList>に同じ */}
       </header>
+      <footer><a href="http://webservice.recruit.co.jp/"><img src="http://webservice.recruit.co.jp/banner/hotpepper-s.gif" alt="ホットペッパー Webサービス" width="135" height="17" border="0" title="ホットペッパー Webサービス"/></a></footer>
     </div>
   );
 }
--- src/OmiseList.js
+++ src/OmiseList.js
@@ -1,16 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const omiseList1 = [
-    "百香亭",
-    "スタバ"
-  ];
+
+// axiosのインスタンス作成
+const KEY =  process.env.API_KEY
+const hotpepper = axios.create ({
+    baseURL: 'https://webservice.recruit.co.jp/hotpepper/large_area/v1/?key=d9a6f98b0866d869&format=json',
+})
+
+// curl https://webservice.recruit.co.jp/hotpepper/large_area/v1/?key=d9a6f98b0866d869&format=json | jq
+// curl https://webservice.recruit.co.jp/hotpepper/large_area/v1/?key=d9a6f98b0866d869&format=json
+const fetchSearchData = async() =>{
+    //大エリアコード=Z011(東京)のお店を検索
+        return await hotpepper.get('')
+}
+
+
+    // params: {
+    //     key:KEY,
+    //     large_area:'Z011',
+    //     format: 'json'  
+    //  }
 
 function OmiseList(){
     const [show, setShow] = useState(false);
+
+    
+    useEffect(() =>{
+        fetchSearchData().then((res) => {
+            console.log(res)
+        })
+    },[])
+
+
+
     return(
         <div>
             <button onClick={() => setShow(true)}>今日のご飯は？</button>
-            {show && <h2>今日のご飯は{omiseList1[Math.floor(Math.random() * omiseList1.length)]}</h2>}
+
+
+
+            {/* {show && <h2>今日のご飯は{omiseList1[Math.floor(Math.random() * omiseList1.length)]}</h2>} */}
         </div>
     );
 }
