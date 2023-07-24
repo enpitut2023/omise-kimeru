2febdaedf9db97812836ddcccdbaa210ae748aec
Mattun1212 <s1911437@s.tsukuba.ac.jp>
2023-07-24 11:28:45 +0900

    [ADD]Styled Componentを追加、リンクを別タブで飛ばせるように、リンクの色を変更

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const genres = ["洋食","和食","中華","イタリアン・フレンチ","焼き肉・ホルモン","お好み焼き・もんじゃ","居酒屋","ラーメン"]
   const budgetOptions =["500","1000","2000","3000","4000","5000"]
 
-  const [filterAttr, setFilterAttr] = useState({"checkedgenres": [], "budget": 500})
+  const [filterAttr, setFilterAttr] = useState({"checkedgenres": [], "budget": 1000})
 
   const [show, setShow] = useState(false);
 
@@ -65,7 +65,7 @@ function App() {
           setFilteredShops(FilterShops(shops, filterAttr));
           setShow(true)
         }}>
-          今日のご飯は？
+          今日のご飯を決める！
         </button>
         {
           show && <SuggestShops filteredShops={ filteredShops } shops = { shops }/>
