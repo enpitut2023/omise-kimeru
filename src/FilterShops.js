ab1cdcb66f8762754bde36b262573c217ab89b9d
Mattun1212 <s2321711@u.tsukuba.ac.jp>
2023-07-24 12:04:20 +0900

    [Fix]価格の未満を以下に

--- src/FilterShops.js
+++ src/FilterShops.js
@@ -5,7 +5,7 @@ function FilterShops(shops, filterAttr){
       return(
           // TODO: shop["budget"]["average"]がnullの場合の処理はtrueにする処理をかく
           // 予算内かどうか
-          shop["budget"]["average"] < filterAttr["budget"] &&
+          shop["budget"]["average"] <= filterAttr["budget"] &&
           //除くべきジャンルかどうか
           !(filterAttr["checkedgenres"].includes(shop["genre"]["name"]))
       );
