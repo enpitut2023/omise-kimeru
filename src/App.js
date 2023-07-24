aa0d43ceead09224f372468009ff9513ca2759de
pictomo <tomo.pict@gmail.com>
2023-07-24 15:47:32 +0900

    ホットペッパーapiの利用

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
-import Data from "./lunch.json";
+import React, { useState, useEffect } from 'react';
 import SuggestShops from './SuggestShops';
 import FilterShops from './FilterShops';
 import Question from './Question';
-
-
+import axios from 'axios';
 
 
 // index.jsから呼ばれる
 function App() {
-  const genres = ["洋食","和食","中華","イタリアン・フレンチ","焼き肉・ホルモン","お好み焼き・もんじゃ","居酒屋","ラーメン"]
-  const budgetOptions =["500","1000","2000","3000","4000","5000"]
+  useEffect(() =>{
+    const url = `https://omise-kimeru-backend.onrender.com/hotpepper-proxy`
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      params: {
+        "large_area": "Z011"
+      }
+    };
+
+    axios.get(url, config).then((res) => {
+      const shops = res["data"]["results"]["shop"]
+      console.log(shops)
+      setShops(shops)
+    })
+  },[]);
+
+  const genres = ["洋食","和食","中華","イタリアン・フレンチ","焼き肉・ホルモン","お好み焼き・もんじゃ","居酒屋","ラーメン"];
+  const budgetOptions =["500","1000","2000","3000","4000","5000"];
 
   const [filterAttr, setFilterAttr] = useState({"checkedgenres": [], "budget": 1000})
-
   const [show, setShow] = useState(false);
-
-  const shops = Data["results"]["shop"];
-
-  const [filteredShops, setFilteredShops] = useState([])
+  const [shops, setShops] = useState([]);
+  const [filteredShops, setFilteredShops] = useState([]);
 
   const handleCheckboxChange= (genre) =>{ //checkboxの入力に基づいて配列にアイテムを加えてる
          setFilterAttr(prevfilterAttr => {
           if (prevfilterAttr.checkedgenres.includes(genre)){
               return{
                   ...prevfilterAttr,
-                  "checkedgenres" :prevfilterAttr.checkedgenres.filter(g => g !=genre)
+                  "checkedgenres" :prevfilterAttr.checkedgenres.filter(g => g !== genre)
               }
           }else{
               return{
@@ -55,7 +67,7 @@ function App() {
           genres={ genres }
           budgetOptions={ budgetOptions }
           filterAttr={ filterAttr }
-             setFilterAttr={    setFilterAttr }
+             setFilterAttr={ setFilterAttr }
           handleCheckboxChange={ handleCheckboxChange }
           handleBudgetChange={ handleBudgetChange }
         />
