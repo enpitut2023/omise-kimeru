56cb114c0b8493e5326c64b00ad3ab69568f2961
MaruiYuta <s2320648@u.tsukuba.ac.jp>
2023-07-25 16:03:53 +0900

    [fix]Header,Footerの色を変更

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 
 function Footer() {
     return (
-        <Navbar fixed="bottom" style={{textAlign:'center',display:'inline-block'}} bg='info'>
+        <Navbar fixed="bottom" style={{textAlign:'center',display:'inline-block',backgroundColor:"burlywood"}}>
                 <Nav.Link href="/home"><a href="http://webservice.recruit.co.jp/"><img src="http://webservice.recruit.co.jp/banner/hotpepper-s.gif" alt="ホットペッパー Webサービス" width="135" height="17" border="0" title="ホットペッパー Webサービス"/></a></Nav.Link>                
         </Navbar>
     );
