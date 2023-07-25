67ff8a23006d73906557f597cc96d4ce7af4db56
pictomo <tomo.pict@gmail.com>
2023-07-25 16:19:35 +0900

    [add]写真の表示を改善

--- src/SuggestShops.js
+++ src/SuggestShops.js
@@ -14,8 +14,6 @@ function ShowShop(props){
     const shopInfo = props.shopInfo;
     return(
         <div>
-            <div>
-
             <StyledLink href={shopInfo["urls"]["pc"]} rel="noopener noreferrer" target="_blank">
                 <h2>{shopInfo["name"]}</h2>
             </StyledLink>
@@ -24,18 +22,21 @@ function ShowShop(props){
                 <p>{ shopInfo["genre"]["catch"] }</p>
             }
             { shopInfo["photo"]["pc"]["l"] &&
-                <img src={ shopInfo["photo"]["pc"]["l"] }/>
+                <img 
+                    src={ shopInfo["photo"]["pc"]["l"] }
+                    style={{
+                        width:"90%",
+                        borderRadius:"5%"
+                    }}
+                />
             }
             { shopInfo["open"] &&
                 <p>{shopInfo["open"]}</p>
             }
-
             { shopInfo["budget"]["average"] === 0 ?
                 <p>予算: 不明</p> :
                 <p>予算: { shopInfo["budget"]["average"] }</p>
             }
-
-            </div>
         </div>
     );
 }
