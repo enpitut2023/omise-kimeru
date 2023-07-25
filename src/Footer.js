9072f5fa86f97522f30dbedcdd3db84e68589f41
MaruiYuta <s2320648@u.tsukuba.ac.jp>
2023-07-25 14:17:29 +0900

    [fix]Header,Footerのレイアウトを変更した

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 function Footer() {
     return (
-        <footer>
-            <a href="http://webservice.recruit.co.jp/"><img src="http://webservice.recruit.co.jp/banner/hotpepper-s.gif" alt="ホットペッパー Webサービス" width="135" height="17" border="0" title="ホットペッパー Webサービス"/></a>
-        </footer>
+        <Navbar fixed="bottom" style={{textAlign:'center',display:'inline-block'}} bg='info'>
+                <Nav.Link href="/home"><a href="http://webservice.recruit.co.jp/"><img src="http://webservice.recruit.co.jp/banner/hotpepper-s.gif" alt="ホットペッパー Webサービス" width="135" height="17" border="0" title="ホットペッパー Webサービス"/></a></Nav.Link>                
+        </Navbar>
     );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 
 function Header() {
-    return (
-        <header>
-            <h1>
-            おみせきめーる
-            </h1>
-        </header>
-    );
+  return (
+    <Navbar expand="lg" bg="warning" variant="dark">
+      <Container className="container-center" style={{ textAlign: "center", display: 'inline-block' }}>
+        <Navbar.Brand href="#" className="fw-bold">おみせきめーる</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Header;
