ca2db79ea50669ebafbe923b94984f416db052ec
Mattun1212 <s1911437@s.tsukuba.ac.jp>
2023-08-08 17:47:04 +0900

    [Add]ボタンを押したらmodalが消えないように

--- src/GeolocationModal.js
+++ src/GeolocationModal.js
@@ -1,7 +1,10 @@
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import React , { useEffect, useState } from 'react';
 
 const GeolocationModal = ({show, handleClose, handleGetGeolocation}) => {
+  const [errorMessage, setErrorMessage] = useState('');
+
   return (
     <>
       <Modal show={show} onHide={handleClose}>
@@ -9,15 +12,21 @@ const GeolocationModal = ({show, handleClose, handleGetGeolocation}) => {
           <Modal.Title>位置情報</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          位置情報を取得できません。
-          位置情報を取得するには、ブラウザの設定を変更してください。
-          このまま継続する場合は、つくばを中心とした検索結果を表示します。
+          {errorMessage ? (
+            <p>{errorMessage}</p>
+          ) : (
+            <>
+              位置情報を取得できません。
+              位置情報を取得するには、ブラウザの設定を変更してください。
+              このまま継続する場合は、つくばを中心とした検索結果を表示します。
+            </>
+          )}
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             閉じる
           </Button>
-          <Button variant="primary" onClick={handleGetGeolocation}>
+          <Button variant="primary" onClick={() => handleGetGeolocation(setErrorMessage)}>
             位置情報を再取得
           </Button>
         </Modal.Footer>
